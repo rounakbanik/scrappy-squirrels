d198b65b5304232311742d796df475243ecd1d5a
Rounak Banik <rounakbanik@gmail.com>
2022-02-12 15:22:18 +0530

    Add Yoda to team

--- src/data/team-data.js
+++ src/data/team-data.js
@@ -1,5 +1,6 @@
 import rony from '../images/team/rony.png';
 import sketchsome from '../images/team/sketchsome.png';
+import yoda from '../images/team/yoda.png';
 
 const TEAM = [
     {
@@ -11,6 +12,12 @@ const TEAM = [
         name: 'Sketchsome',
         twitter: 'https://twitter.com/MuskanRaina97',
         avatar: sketchsome,
+    },
+    {
+        name: 'Yoda',
+        twitter: 'https://twitter.com/ankg404',
+        avatar: yoda,
+
     }
 ]
 
